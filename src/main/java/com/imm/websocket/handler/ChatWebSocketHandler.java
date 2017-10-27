80c246fa94a2ee6a63e78aeb2ccecf384e172074
zhanglong <misnearzhang@gmail.com>
2017-10-28 00:44:00 +0800

    chang the code style

--- src/main/java/com/imm/websocket/handler/ChatWebSocketHandler.java
+++ src/main/java/com/imm/websocket/handler/ChatWebSocketHandler.java
@@ -23,7 +23,7 @@ import java.util.List;
 public class ChatWebSocketHandler extends TextWebSocketHandler {
     private final Logger logger = LogManager.getLogger( ChatWebSocketHandler.class );
     private final static List<WebSocketSession> sessions = Collections.synchronizedList(new ArrayList<WebSocketSession>());
-    //接收文本消息，并发送出去
+
     @Override
     protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
         logger.info(new String(message.asBytes()));
@@ -49,15 +49,12 @@ public class ChatWebSocketHandler extends TextWebSocketHandler {
         logger.info(message);
         super.handleTextMessage(session, message);
     }
-    //连接建立后处理
     @SuppressWarnings("unchecked")
     @Override
     public void afterConnectionEstablished(WebSocketSession session) throws Exception {
         logger.debug("connect to the websocket chat success......");
         sessions.add(session);
-        //处理离线消息
     }
-    //抛出异常时处理
     @Override
     public void handleTransportError(WebSocketSession session, Throwable exception) throws Exception {
         if(session.isOpen()){
@@ -66,7 +63,6 @@ public class ChatWebSocketHandler extends TextWebSocketHandler {
         logger.debug("websocket chat connection closed......");
         sessions.remove(session);
     }
-    //连接关闭后处理
     @Override
     public void afterConnectionClosed(WebSocketSession session, CloseStatus closeStatus) throws Exception {
         logger.debug("websocket chat connection closed......");

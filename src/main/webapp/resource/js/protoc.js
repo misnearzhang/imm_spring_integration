1ca71544bb8014666c767dea12bb78cd1b48cced
zhanglong <123456>
2017-12-24 11:50:25 +0800

    chang the code style

--- src/main/webapp/resource/js/protoc.js
+++ src/main/webapp/resource/js/protoc.js
@@ -13,3 +13,11 @@ var header = {
     "status":"",// {请求消息 1. 100:请求信息}    {响应消息  2.200:响应正常(登录验证的响应为有数据响应)     3.404:响应无数据(登录验证时响应无数据)    4.500:响应服务器报错}
     "uid":""//消息编码  响应的消息该字段为原消息编码
 }
+
+function header(header,header,uid)
+{
+    this.header()=headerfirstname;
+    this.lastname=lastname;
+    this.age=age;
+    this.eyecolor=eyecolor;
+}
